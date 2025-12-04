d27aa229709f0aa5590940a92ca6e3d0e2fa7204
hw1425 <wuhaotian198321@outlook.com>
2025-12-04 21:24:32 +0000

    refactor yolov8

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     model.train(
         data='VisDrone.yaml',  # 指向你之前配置好的数据集 yaml
         epochs=100,               # 轮数
-        imgsz=640,                # 图像大小
+        imgsz=960,                # 图像大小
         project='runs/train',     # 保存路径
         amp=True,
     )
